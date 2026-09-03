12d8e3c857201965826c5680ad600795dfc84b6f
keerthi <424146@student.nitandhra.ac.in>
2026-09-03 11:10:41 +0530

    Add Review and Notification models with admin registration, customize admin site header

--- travelbooking/settings.py
+++ travelbooking/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notifications',
+    'accounts',
+    'bookings',
+    'buses',
+    'flights',
+    'hotels',
+    'payments',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +82,14 @@ WSGI_APPLICATION = 'travelbooking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel_booking_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Keerthi@2007',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
